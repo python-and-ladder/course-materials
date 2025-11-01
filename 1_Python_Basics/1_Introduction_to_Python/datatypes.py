3df8d7dc6aee4e0412a0a2cf3c2e4274802c7060
revantharunachalam <revanth.sbioa@gmail.com>
2025-11-01 18:17:30 +0530

    [https://github.com/python-and-ladder/course-materials/issues/5]: Added string 2 changes

--- 1_Python_Basics/1_Introduction_to_Python/datatypes.py
+++ 1_Python_Basics/1_Introduction_to_Python/datatypes.py
@@ -9,3 +9,7 @@ print("Float:", b, "Type:", type(b))
 # String
 c = "Hello, Python!"
 print("String:", c, "Type:", type(c))
+
+# String 2
+d = 'Python is fun!'
+print("String 2:", d, "Type:", type(d))
